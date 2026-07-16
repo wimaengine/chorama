0dc1e026190878aad4804c47779d929d0666c5f9
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-07-16 10:56:27 +0300

    Add example for `helix`

---
+++ examples/mesh/helix.js
@@ -0,0 +1,131 @@
+import Stats from "stats.js"
+import { GUI } from "dat.gui"
+import {
+  BasicMaterial,
+  Camera,
+  CameraPlugin,
+  HelixMeshBuilder,
+  MeshMaterial3D,
+  MeshMaterialPlugin,
+  PerspectiveProjection,
+  OrbitCameraControls,
+  TextureLoader,
+  WireframeBuilder,
+  WebGLRenderDevice,
+  WebGLRenderer,
+  CanvasTarget
+} from "chorama"
+
+const stats = new Stats()
+stats.showPanel(1)
+document.body.append(stats.dom)
+stats.dom.removeAttribute("style")
+stats.dom.classList.add("performance-monitor")
+
+const canvas = document.createElement("canvas")
+const renderTarget = new CanvasTarget(canvas)
+const renderDevice = new WebGLRenderDevice(canvas, {
+  depth: true
+})
+const renderer = new WebGLRenderer({
+  plugins: [
+    new CameraPlugin(),
+    new MeshMaterialPlugin(),
+  ]
+})
+const camera = new Camera(renderTarget)
+const cameraControls = new OrbitCameraControls(camera, canvas)
+
+const textureLoader = new TextureLoader()
+const texture = textureLoader.load({
+  paths: ["/images/uv.jpg"],
+  flipY: true
+})
+const material = new BasicMaterial({
+  mainTexture: texture
+})
+
+const meshBuilder = new HelixMeshBuilder()
+meshBuilder.radius = 0.45
+meshBuilder.tubeRadius = 0.055
+meshBuilder.turns = 4
+meshBuilder.height = 1.95
+meshBuilder.radialSegments = 10
+meshBuilder.pathSegments = 160
+const object = new MeshMaterial3D(meshBuilder.build(), material)
+
+camera.transform.position.set(0, 0.35, 8)
+if (camera.projection instanceof PerspectiveProjection) {
+  camera.projection.fov = Math.PI / 180 * 100
+  camera.projection.aspect = innerWidth / innerHeight
+}
+
+document.body.append(canvas)
+addEventListener("resize", updateView)
+updateView()
+requestAnimationFrame(update)
+
+const controls = new GUI()
+const settings = {
+  wireframe: false
+}
+
+const buildOptionsFolder = controls.addFolder("Settings")
+buildOptionsFolder
+  .add(meshBuilder, "radius", 0.1, 2, 0.01)
+  .name("Radius")
+  .onFinishChange(buildMesh)
+buildOptionsFolder
+  .add(meshBuilder, "tubeRadius", 0.01, 0.5, 0.001)
+  .name("Tube Radius")
+  .onFinishChange(buildMesh)
+buildOptionsFolder
+  .add(meshBuilder, "turns", 1, 12, 1)
+  .name("Turns")
+  .onFinishChange(buildMesh)
+buildOptionsFolder
+  .add(meshBuilder, "height", 0.1, 6, 0.01)
+  .name("Height")
+  .onFinishChange(buildMesh)
+buildOptionsFolder
+  .add(meshBuilder, "radialSegments", 3, 32, 1)
+  .name("Radial Segments")
+  .onFinishChange(buildMesh)
+buildOptionsFolder
+  .add(meshBuilder, "pathSegments", 8, 512, 1)
+  .name("Path Segments")
+  .onFinishChange(buildMesh)
+buildOptionsFolder
+  .add(settings, "wireframe")
+  .name("Wireframe")
+  .onChange(buildMesh)
+buildOptionsFolder.open()
+
+function update() {
+  stats.begin()
+  cameraControls.update()
+  renderer.render([object, camera], renderDevice)
+  stats.end()
+
+  requestAnimationFrame(update)
+}
+
+function updateView() {
+  canvas.style.width = innerWidth + "px"
+  canvas.style.height = innerHeight + "px"
+  canvas.width = innerWidth * devicePixelRatio
+  canvas.height = innerHeight * devicePixelRatio
+
+  if (camera.projection instanceof PerspectiveProjection) {
+    camera.projection.aspect = innerWidth / innerHeight
+  }
+}
+
+function buildMesh() {
+  const mesh = meshBuilder.build()
+  if (settings.wireframe) {
+    object.mesh = new WireframeBuilder(mesh).build()
+  } else {
+    object.mesh = mesh
+  }
+}
